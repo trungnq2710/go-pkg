e8370177b35839050c401b019a3e29d84e7c6099
trungnq2710 <trungnq2710@gmail.com>
2022-03-24 17:26:38 +0700

    feat: add string util

--- pkg/util/xstring/string.go
+++ pkg/util/xstring/string.go
@@ -4,10 +4,39 @@
 package xstring
 
 import (
+	"crypto/rand"
+	"math/big"
 	"reflect"
 	"runtime"
+	"unicode"
 )
 
 func FunctionName(i interface{}) string {
 	return runtime.FuncForPC(reflect.ValueOf(i).Pointer()).Name()
+}
+
+func IsUniqueCharacters(chars []byte) bool {
+	seen := make(map[byte]bool)
+	for _, char := range chars {
+		if _, ok := seen[char]; ok {
+			return false
+		}
+		seen[char] = true
+	}
+	return true
+}
+
+func GenerateRandomString(length int) (string, error) {
+	var result string
+	for len(result) < length {
+		num, err := rand.Int(rand.Reader, big.NewInt(int64(127)))
+		if err != nil {
+			return "", err
+		}
+		n := num.Int64()
+		if unicode.IsLetter(rune(n)) {
+			result += string(n)
+		}
+	}
+	return result, nil
 }
